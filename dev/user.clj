f4f9acc12acde47f3a8ea8c5348f8eee0ef9f8ce
Luke VanderHart <luke@vanderhart.net>
2017-05-02 11:07:55 -0400

    use component.repl instead of reloaded.repl

--- dev/user.clj
+++ dev/user.clj
@@ -1,16 +1,37 @@
 (ns user
-  (:require [reloaded.repl :refer [set-init! system init start stop go reset reset-all]]
-            [arachne.core :as a]
-            [arachne.figwheel :as fig]
-            [org.arachne-framework.template.enterprise-spa :as app]))
+  "Tools for interactive development with the REPL. This file should
+  not be included in a production build of the application.
+  Call `(reset)` to reload modified code and (re)start the system.
+  The system under development is `system`, referred from
+  `com.stuartsierra.component.repl/system`.
+  See also https://github.com/stuartsierra/component.repl"
+  (:require
+    [clojure.java.io :as io]
+    [clojure.java.javadoc :refer [javadoc]]
+    [clojure.pprint :refer [pprint]]
+    [clojure.reflect :refer [reflect]]
+    [clojure.repl :refer [apropos dir doc find-doc pst source]]
+    [clojure.set :as set]
+    [clojure.string :as string]
+    [clojure.test :as test]
+    [clojure.tools.namespace.repl :refer [refresh refresh-all clear]]
+    [com.stuartsierra.component :as component]
+    [com.stuartsierra.component.repl :refer [reset set-init start stop system]]
+    [arachne.core :as a]
+    [arachne.figwheel :as fig]
+    [org.arachne-framework.template.enterprise-spa :as app]))
 
-(defn init-arachne
-  "Create an Arachne runtime"
-  [runtime]
-  (a/runtime (a/config :org.arachne-framework.template/enterprise-spa)
-             runtime))
+;; Do not try to load source code from 'resources' or 'config' directory
+(clojure.tools.namespace.repl/set-refresh-dirs "dev" "src" "test")
 
-(set-init! #(init-arachne ::app/runtime))
+(defn dev-system
+  "Constructs a system map suitable for interactive development."
+  []
+  (a/runtime
+    (a/config :org.arachne-framework.template/enterprise-spa)
+    ::app/runtime))
+
+(set-init (fn [_] (dev-system)))
 
 (defn cljs-repl
   "Launch a CLJS repl for a Figwheel in the currently running Arachne system."
--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject org.arachne-framework.template/enterprise-spa "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
 
-                 [org.arachne-framework/pedestal-assets "0.2.0-master-0011-b830d3d"]
-                 [org.arachne-framework/arachne-figwheel "0.2.0-master-0021-b0fb11f"]
+                 [org.arachne-framework/pedestal-assets "0.2.0-master-0014-8fd438f"]
+                 [org.arachne-framework/arachne-figwheel "0.2.0-master-0025-56f212b"]
+                 [org.arachne-framework/arachne-sass "0.2.0-master-0024-51d4bce"]
 
                  [com.datomic/datomic-free "0.9.5561"
                   :exclusions [org.slf4j/slf4j-api
@@ -18,14 +19,16 @@
                  [ch.qos.logback/logback-classic "1.1.3"
                   :exclusions [org.slf4j/slf4j-api]]
 
-                 [rum "0.10.8"]]
+                 [rum "0.10.8"]
+                 [org.webjars/bootstrap "3.3.7-1"]]
   :source-paths ["src" "config"]
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[com.cemerick/piggieback "0.2.1"
                                    :exclusions [org.clojure/clojurescript]]
-                                  [reloaded.repl "0.2.3"
-                                   :exclusions [com.stuartsierra/component]]]
-                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
+                                  [com.stuartsierra/component.repl "0.2.0"]
+                                  [org.clojure/tools.namespace "0.2.11"]]
+                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+                   :main user}}
   :main arachne.run
   :repositories [["arachne-dev"
                   "http://maven.arachne-framework.org/artifactory/arachne-dev"]])
