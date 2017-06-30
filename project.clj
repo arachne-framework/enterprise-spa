90280fb0d89940acf68f0c03830b8008503e2fd0
Luke VanderHart <luke@vanderhart.net>
2017-06-30 13:31:57 -0400

    Allow uberjaring. Fixes #4

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject org.arachne-framework.template/enterprise-spa "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
 
-                 [org.arachne-framework/pedestal-assets "0.2.0-master-0014-8fd438f"]
-                 [org.arachne-framework/arachne-figwheel "0.2.0-master-0025-56f212b"]
-                 [org.arachne-framework/arachne-sass "0.2.0-master-0024-51d4bce"]
+                 [org.arachne-framework/pedestal-assets "0.2.0-master-0018-b7669fc"]
+                 [org.arachne-framework/arachne-figwheel "0.2.0-master-0030-676b9a2"]
+                 [org.arachne-framework/arachne-sass "0.2.0-master-0033-bd94dca"]
 
                  [com.datomic/datomic-free "0.9.5561"
                   :exclusions [org.slf4j/slf4j-api
@@ -28,7 +28,11 @@
                                    :exclusions [org.clojure/clojurescript]]
                                   [com.stuartsierra/component.repl "0.2.0"]
                                   [org.clojure/tools.namespace "0.2.11"]]
-                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
+                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}
+             :uberjar {:uberjar-merge-with {"arachne.edn" [(comp read-string slurp) into #(spit %1 (pr-str %2))]}
+                       :aot [arachne.run]}}
+
   :main arachne.run
   :repositories [["arachne-dev"
-                  "http://maven.arachne-framework.org/artifactory/arachne-dev"]])
+                  "http://maven.arachne-framework.org/artifactory/arachne-dev"]]
+  )
